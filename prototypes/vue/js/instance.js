8e33f628be4f9f8199a880baf4bf7242ac261b7b
Maximilian Brandl <kontakt@brandl-maximilian.de>
2017-11-12 22:29:05 +0100

    Vue Basic Test

---
+++ prototypes/vue/js/instance.js
@@ -0,0 +1,11 @@
+// https://vuejs.org/v2/guide/index.html -> Composing with Components
+var data = {
+    title : "Hello World from vue",
+    para : "I dont know what Im doin",
+    class : "classname"
+}
+
+var vue = new Vue({
+    el: '#app',
+    data: data
+})
